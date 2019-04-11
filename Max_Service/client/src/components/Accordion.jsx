4fbe0dad4de28e1102a900cf04a961f77d05ddff
hunterxdong <hunterdong123@gmail.com>
2019-04-10 22:16:36 -0700

    renders reviews, description, and suggestions

---
+++ Max_Service/client/src/components/Accordion.jsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import styled from 'styled-components';
+import Modal from 'react-modal';
+import Reviews from './reviews.jsx';
+import ReviewsPreview from './reviewspreview.jsx'
+import ModalAllReviews from './Modal.allreviews.jsx'
+import InputForm from './Modal.writereview.jsx'
+Modal.setAppElement('#app')
+import $ from 'jquery';
+
+
+
+class Accordion extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      open:false,
+      modal:false,
+      reviewsfromdb:[]
+    }
+    this.toggleOpen = this.toggleOpen.bind(this)
+    this.toggleModal = this.toggleModal.bind(this)
+    this.updatefunction = this.updatefunction.bind(this)
+  }
+
+  componentDidMount(){
+    console.log('componenet mount')
+    var path = window.location.pathname
+    console.log(path.slice(7))
+    var prodid = path.slice(7)
+
+    $.ajax({
+      url:`/shoes/${prodid}reviews`,
+      method:'GET',
+      success:(data)=>{this.setState({
+        reviewsfromdb:data
+      })}
+    })
+  }
+
+  toggleOpen(){
+    console.log(this.state.reviewsfromdb)
+    this.setState({
+      open: !this.state.open
+    })
+  }
+  toggleModal(){
+    console.log('toggle modal')
+    this.setState({
+      modal: !this.state.modal
+    })
+  }
+
+  //this function passed into inputform as callback to update reviewsfrom db state after form submission
+  updatefunction(){
+    var path = window.location.pathname
+    console.log(path.slice(7))
+    var prodid = path.slice(7)
+
+    $.ajax({
+      url:`/shoes/${prodid}reviews`,
+      method:'GET',
+      success:(data)=>{this.setState({
+        reviewsfromdb:data
+      })}
+    })
+
+  }
+
+
+  render(){
+    return(
+      <div>
+        <Button onClick = {this.toggleOpen}>Reviews ({this.state.reviewsfromdb.length})</Button>
+
+          <ReviewContent open = {this.state.open} >
+
+          <InputForm updatefunction = {this.updatefunction}/>
+
+          <ReviewsPreview allreviews = {this.state.reviewsfromdb}/>
+
+
+          <ModalAllReviews
+            allreviews = {this.state.reviewsfromdb}
+          />
+
+
+        </ReviewContent>
+      </div>
+    )
+
+  }
+}
+
+//COMPONENT STYLE
+var Button = styled.button`
+  background-color: #eee;
+  color: #444;
+  cursor: pointer;
+  padding: 18px;
+  width: 500px;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+  transition: 0.4s;
+`;
+
+var ReviewContent = styled.div`
+  border: 0px solid gray;
+  border-top: none;
+  opacity: ${props => (props.open ? "1" : "0")};
+  max-height: ${props => (props.open ? "100%" : "0")};
+  overflow: hidden;
+  padding: ${props => (props.open ? "15px" : "0 15px")};
+  transition: all 0.3s;
+`
+
+export default Accordion
--- proxy/server/server.js
+++ proxy/server/server.js
@@ -1,12 +1,31 @@
 const express = require("express");
-//const morgan = require("morgan");
+const proxy = require("http-proxy-middleware");
 const path = require("path");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const app = express();
 const port = process.env.Prt || 5000;
 
+app.use(cors());
 app.use(express.static(__dirname + "/../public"));
+
+const suggestionsProxy = proxy({
+  target: "http://localhost:3000",
+  changeOrigin: true
+});
+app.use("/api/suggestions", suggestionsProxy);
+
+const descriptionProxy = proxy({
+  target: "http://localhost:4000",
+  changeOrigin: true
+});
+app.use("/api/description", descriptionProxy);
+
+const reviewsProxy = proxy({
+  target: "http://localhost:7000/shoes/2",
+  changeOrigin: true
+});
+app.use("/shoes/2", reviewsProxy);
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
