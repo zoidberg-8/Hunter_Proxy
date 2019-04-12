e4730aee8f38f818e28aca8c032fe8c83f230c3d
hunterxdong <hunterdong123@gmail.com>
2019-04-11 23:21:28 -0700

    proxy works with dynamic endpoints also added simple navbar

--- proxy/server/server.js
+++ proxy/server/server.js
@@ -7,25 +7,30 @@ const app = express();
 const port = process.env.Prt || 5000;
 
 app.use(cors());
+
 app.use(express.static(__dirname + "/../public"));
+
+app.use("/shoes/:id", express.static(__dirname + "/../public"));
+
+app.use("/shoes/", express.static(__dirname + "/../public"));
 
 const suggestionsProxy = proxy({
   target: "http://localhost:3000",
   changeOrigin: true
 });
-app.use("/api/suggestions", suggestionsProxy);
+app.use("/shoes/:id/suggestions", suggestionsProxy);
 
 const descriptionProxy = proxy({
   target: "http://localhost:4000",
   changeOrigin: true
 });
-app.use("/api/description", descriptionProxy);
+app.use("/shoes/:id/description", descriptionProxy);
 
 const reviewsProxy = proxy({
-  target: "http://localhost:7000/shoes/2",
+  target: "http://localhost:7000",
   changeOrigin: true
 });
-app.use("/shoes/2", reviewsProxy);
+app.use("/shoes/:id/reviews", reviewsProxy);
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
